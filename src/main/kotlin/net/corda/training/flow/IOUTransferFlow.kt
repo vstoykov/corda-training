8ae32c2080954e01b6fc8cd89659a37726ffcb82
Venelin Stoykov <venelin.stoykov@industria.tech>
2018-08-08 12:13:39 +0100

    Transfer Flow

--- src/main/kotlin/net/corda/training/flow/IOUTransferFlow.kt
+++ src/main/kotlin/net/corda/training/flow/IOUTransferFlow.kt
@@ -1,8 +1,7 @@
 package net.corda.training.flow
 
 import co.paralleluniverse.fibers.Suspendable
-import net.corda.core.contracts.UniqueIdentifier
-import net.corda.core.contracts.requireThat
+import net.corda.core.contracts.*
 import net.corda.core.flows.CollectSignaturesFlow
 import net.corda.core.flows.FinalityFlow
 import net.corda.core.flows.FlowLogic
@@ -12,8 +11,11 @@ import net.corda.core.flows.InitiatingFlow
 import net.corda.core.flows.SignTransactionFlow
 import net.corda.core.flows.StartableByRPC
 import net.corda.core.identity.Party
+import net.corda.core.node.services.queryBy
+import net.corda.core.node.services.vault.QueryCriteria
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
+import net.corda.training.contract.IOUContract
 import net.corda.training.state.IOUState
 
 /**
@@ -27,10 +29,28 @@ import net.corda.training.state.IOUState
 class IOUTransferFlow(val linearId: UniqueIdentifier, val newLender: Party): FlowLogic<SignedTransaction>() {
     @Suspendable
     override fun call(): SignedTransaction {
-        // Placeholder code to avoid type error when running the tests. Remove before starting the flow task!
-        return serviceHub.signInitialTransaction(
-                TransactionBuilder(notary = null)
-        )
+        val notary = serviceHub.networkMapCache.notaryIdentities.first()
+        val txBuilder = TransactionBuilder(notary = notary)
+        val stateAndRef = serviceHub.vaultService.queryBy<IOUState>(
+            QueryCriteria.LinearStateQueryCriteria(
+                linearId = listOf(linearId)
+            )
+        ).states.single()
+        val input = stateAndRef.state.data
+        if (input.lender != ourIdentity) {
+            throw IllegalArgumentException("Flow can only be run by current lender")
+        }
+        val output = input.withNewLender(newLender)
+        val participants = input.participants.toSet() + output.participants.toSet()
+        val command = Command(IOUContract.Commands.Transfer(), participants.map { it.owningKey })
+
+        txBuilder.withItems(command, stateAndRef, StateAndContract(output, IOUContract.IOU_CONTRACT_ID))
+        txBuilder.verify(serviceHub)
+
+        val sessions = (participants - ourIdentity).map { initiateFlow(it) }
+        val tx = serviceHub.signInitialTransaction(txBuilder)
+        val stx = subFlow(CollectSignaturesFlow(tx, sessions))
+        return subFlow(FinalityFlow(stx))
     }
 }
 
